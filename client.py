abca6671dc9aab4aef315343df084d0fbfebe875
sohan9819 <64985447+sohan9819@users.noreply.github.com>
2021-01-23 23:58:07 +0530

    Update client.py

--- client.py
+++ client.py
@@ -6,34 +6,17 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-# server = smtplib.SMTP_SSL('smtp.gmail.com', 25)
-# server.connect("smtp.example.com", 465)
-# server.ehlo()
-# server.starttls()
-# server.ehlo()
-
-# with open('password.txt', 'r') as f:
-#     password = f.read()
-
-# server.login('sohanshetty2001@gmail.com', password)
-
-######################################################################
-
-# cwd = os.getcwd()  # Get the current working directory (cwd)
-# files = os.listdir(cwd)  # Get all the files in that directory
-# print("Files in %r: %s" % (cwd, files))
-
 ######################################################################
 
 server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 
-server.login("sohanshetty2001@gmail.com", config('PASSWORD'))
+server.login("sender@gmail.com", config('PASSWORD'))
 
 ######################################################################
 
 msg = MIMEMultipart()
-msg["From"] = 'sohanshetty2001@gmail.com'
-msg['To'] = '2019sohan.shetty@ves.ac.in'
+msg["From"] = 'sender@gmail.com'
+msg['To'] = 'reciever@gmail.com'
 msg['Subject'] = 'Just A test'
 
 with open('message.txt', 'r') as f:
@@ -53,7 +36,7 @@ p.add_header('Content-Disposition', f'attachment; filename={filename}')
 msg.attach(p)
 
 text = msg.as_string()
-server.sendmail("sohanshetty2001@gmail.com",
-                "2019sohan.shetty@ves.ac.in", text)
+server.sendmail("sender@gmail.com",
+                "reciever@gmail.com", text)
 
 server.quit()
